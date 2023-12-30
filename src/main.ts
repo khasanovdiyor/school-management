fbf61637630ec759a43685d87393ebdfbeaef2c0
Diyor Khasanov <diyor.khasanov.dev@gmail.com>
2023-12-30 17:48:59 +0500

    add global validation pipe, interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './common/interceptors/transform.interseptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
